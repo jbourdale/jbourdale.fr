16d3d68ae8a8cc936159fea75c57d67ab6748988
Jules Bourdalé <jules.bourdale@gmail.com>
2021-08-21 11:10:57 +0200

    feat(clear): clear everything, starting from blank

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from jbourdale.fr'"; //letter subject
-	$to = 'jules.bourdale@gmail.com'; // Enter Your E-mail
-	$from = 'contact@jbourdale.fr'; // Admin e-mail
-	$fromName = '[JBOURDALE.FR]'; // Your company name
-	$charset = 'UTF-8';
-?>
