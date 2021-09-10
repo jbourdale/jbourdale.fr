59959bd4230748685cf7ff98b4606ddec36b634f
Jules Bourdalé <jules.bourdale@gmail.com>
2021-09-10 17:53:51 +0200

    wip

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,3 +1,6 @@
+const careerTitleEl = document.getElementById('career-title');
+const yearEventEls = document.getElementsByClassName('year-event');
+
 const debounce = (fn) => {
   let queued
   return function (...args) {
@@ -9,6 +12,22 @@ const debounce = (fn) => {
   }
 }
 
+const isInViewport = (el) => {
+    const rect = el.getBoundingClientRect();
+    console.log("isInViewPort, comparing")
+    console.log("rect.top >= 0", rect.top)
+    console.log("rect.left >= 0", rect.left)
+    console.log("rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)", rect.bottom, window.innerHeight || document.documentElement.clientHeight)
+    console.log("rect.right <= (window.innerWidth || document.documentElement.clientWidth)", rect.right, (window.innerWidth || document.documentElement.clientWidth))
+    return (
+        rect.top >= -200 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+
 const scrollHandler = () => {
   const { scrollY } = window;
 
@@ -16,6 +35,18 @@ const scrollHandler = () => {
 
   if (scrollY > 85) { document.getElementsByClassName('header')[0].classList.add('detached') }
   if (scrollY < 85) { document.getElementsByClassName('header')[0].classList.remove('detached') }
+  if (isInViewport(careerTitleEl)) { careerTitleEl.classList.add('tracking-in-expand-fwd-bottom') }
+
+  // Sliding year event from right or left
+  for (const yearEventEl of yearEventEls) {
+    if (isInViewport(yearEventEl)) {
+      if (yearEventEl.classList.contains('rtl')) {
+        yearEventEl.classList.add('slide-in-right')
+      } else {
+        yearEventEl.classList.add('slide-in-left')
+      }
+    }
+  }
 }
 
 document.addEventListener('scroll', debounce(scrollHandler), { passive: true });
